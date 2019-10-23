f70c916dbb7d259d4329b11f98a5b653903c96c4
Haerden <719042341@qq.com>
2019-10-23 22:17:08 +0800

    feat: koa-parameter

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,7 +2,6 @@ const db = [{ name: "li lei" }];
 
 class UsersCtl {
     find(ctx) {
-        a.b
         ctx.body = db;
     }
 
@@ -11,16 +10,29 @@ class UsersCtl {
     }
 
     create(ctx) {
+        ctx.verifyParams({
+            name: { type: 'string', required: true },
+            age: { type: 'number', required: false }
+        })
+
         db.push(ctx.request.body);
         ctx.body = ctx.request.body;
     }
 
     update(ctx) {
+        if (ctx.params.id >= db.length) {
+            ctx.throw(412);
+        }
+
         db[ctx.params.id * 1] = ctx.request.body;
         ctx.body = ctx.request.body;
     }
 
     delete(ctx) {
+        if (ctx.params.id >= db.length) {
+            ctx.throw(412);
+        }
+
         db.splice(ctx.params.id * 1, 1);
         ctx.status = 204; //204 代表没有内容
     }
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const parameter = require('koa-parameter');
 const error = require('koa-json-error');
 const app = new Koa();
 
@@ -9,6 +10,7 @@ app.use(error({
     postFormat: (e, { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
 }));
 app.use(bodyParser()); // 获取请求body
+app.use(parameter(app)); // 检验参数
 routing(app);
 
 app.listen(4000, () => console.log('is runnning at PORT 4000'));
