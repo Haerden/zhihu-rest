680e988e5ccf4a6c68aeb7a1e0c918e59edda667
Haerden <719042341@qq.com>
2019-10-31 13:53:33 +0800

    feat: add delete,auth,token

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,29 @@
+const jsonwebtoken = require('jsonwebtoken');
 const Router = require('koa-router');
 // const router = new Router({prefix: '/users'});
 const router = new Router();
 const { find, findById, update, create,
     delete: del, login
 } = require('../controllers/users');
+
+const { secret } = require('../config');
+
+// auth
+const auth = async (ctx, next) => {
+    const { authorization = '' } = ctx.request.header;
+
+    const token = authorization.replace('Bearer ', '');
+
+    try {
+        // 验证token有没有被篡改过
+        const user = jsonwebtoken.verify(token, secret);
+        ctx.state.user = user;
+    } catch (error) {
+        ctx.throw(401, error.message);
+    }
+
+    await next();
+}
 
 router.get('/users', find);
 
@@ -14,7 +34,7 @@ router.patch('/users/:id', update);
 
 router.post('/users', create);
 
-router.delete('/users/:id', del);
+router.delete('/users/:id', auth, del); //认证＝>授权
 
 router.post('/users/login', login);
 
