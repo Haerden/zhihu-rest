ac9faf3d6f4fc98440e2a12a0595b879e9bce791
Haerden <719042341@qq.com>
2019-11-02 16:53:23 +0800

    个人信息Schema 设计

--- app/model/users.js
+++ app/model/users.js
@@ -5,7 +5,26 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
     __v: { type: Number, select: false },
     name: { type: String, required: true },
-    password: { type: String, required: true, select: true }
+    password: { type: String, required: true, select: true },
+    avatar_url: { type: String },
+    gender: { type: String, enum: ['male', 'female'], default: 'male' },
+    locations: { type: [{ type: String }] },
+    business: { type: String },
+    employments: {
+        type: [{
+            company: { type: String },
+            jos: { type: String }
+        }]
+    },
+    educations: {
+        type: [{
+            school: { type: String },
+            major: { type: String },
+            diploma: { type: Number, enum: [1, 2, 3, 4, 5] }, // 学历
+            entrance_year: { type: Number },
+            graduation_year: { type: Number }
+        }]
+    }
 });
 
 module.exports = model('User', userSchema);
