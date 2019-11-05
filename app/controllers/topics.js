c1f7152715f35a2d8d054c2debd4761ad578e544
Haerden <719042341@qq.com>
2019-11-05 09:24:31 +0800

    话题列表 增改查

--- app/controllers/topics.js
+++ app/controllers/topics.js
@@ -1,9 +1,47 @@
+//1 schma 2 controls 3 路由
 const Topic = require('../model/topics');
 // const { secret } = require('../config');
 
 class TopicsCtl {
     async find(ctx) {
         ctx.body = await Topic.find();
+    }
+
+    async findById(ctx) {
+        const { fields } = ctx.query;
+
+        const selectFields = fields.split(';').filter(f => f).map(f => `+${f}`).join('');
+
+        const topic = await Topic.findById(ctx.params.id).select(selectFields);
+        ctx.body = topic;
+
+    }
+
+    async create(ctx) {
+        // 校验请求体
+        ctx.verifyParams({
+            name: { type: 'string', required: true },
+            avatar_url: { type: 'string', required: false },
+            introduction: { type: 'string', required: false }
+        });
+
+        const topic = await new Topic(ctx.request.body).save(); // 添加要save
+
+        ctx.body = topic;
+    }
+
+    async update(ctx) {
+        // 校验请求体
+        ctx.verifyParams({
+            name: { type: 'string', required: false },
+            avatar_url: { type: 'string', required: false },
+            introduction: { type: 'string', required: false }
+        });
+
+        const topic = Topic.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+
+        ctx.body = topic; //(更新前的topic)
+
     }
 
 }
---
+++ app/routes/topics.js
@@ -0,0 +1,23 @@
+const jwt = require('koa-jwt');
+const Router = require('koa-router');
+const router = new Router({ prefix: '/topics' });
+const router = new Router();
+
+const { find, findById, update, create,
+
+} = require('../controllers/topics');
+
+const { secret } = require('../config');
+
+const auth = jwt({ secret });
+
+router.get('/', find);
+
+router.post('/', auth, create);
+
+router.get('/:id', findById);
+
+router.patch('/:id', auth, update);
+
+
+module.exports = router;
